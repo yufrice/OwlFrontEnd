44b9cbffabcb3570927b40e27b0561ddb2234327
yufrice <c1155208@cstu.nit.ac.jp>
2018-11-06 01:38:30 +0900

    Add index.ts

---
+++ src/components/Atoms/index.ts
@@ -0,0 +1,6 @@
+import { Button } from './Button';
+import { TextInput } from './Input';
+import { theme } from './Theme';
+import { Typography } from './Typography';
+
+export { Button, TextInput, theme, Typography}
---
+++ src/components/Molecules/index.ts
@@ -0,0 +1,5 @@
+import { Card } from './Card';
+import { IconButton } from './IconButton';
+import { Modal } from './Modal';
+
+export { Card, IconButton, Modal }
