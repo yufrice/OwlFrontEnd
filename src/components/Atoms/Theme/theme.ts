e389ccc23f19103867bf994060096d890758fbf5
yufrice <c1155208@cstu.nit.ac.jp>
2018-11-07 19:42:25 +0900

    fix deprecation api

--- src/components/Atoms/Theme/theme.ts
+++ src/components/Atoms/Theme/theme.ts
@@ -2,6 +2,9 @@ import * as Color from '@material-ui/core/colors';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 export const theme = createMuiTheme({
+    typography: {
+        useNextVariants: true,
+    },
     palette: {
         primary: Color.blue,
         secondary: {
--- src/components/Atoms/index.ts
+++ src/components/Atoms/index.ts
@@ -1,6 +1,5 @@
 import { Button } from './Button';
 import { TextInput } from './Input';
 import { theme } from './Theme';
-import { Typography } from './Typography';
 
-export { Button, TextInput, theme, Typography}
+export { Button, TextInput, theme }
