c6abeb155edfc8acfc78e264725ec431b0235646
yufrice <c1155208@cstu.nit.ac.jp>
2018-11-08 01:32:29 +0900

    Add domain index

--- src/components/Molecules/index.ts
+++ src/components/Molecules/index.ts
@@ -1,5 +1,6 @@
 import { IconButton } from './IconButton';
 import { ItemCard } from './ItemCard';
 import { Modal } from './Modal';
+import { SearchForm } from './SearchForm';
 
-export { ItemCard, IconButton, Modal }
+export { ItemCard, IconButton, Modal, SearchForm }
