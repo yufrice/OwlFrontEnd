07c50d5776a9aaa2a57b141f9de828e439dd9593
yufrice <c1155208@cstu.nit.ac.jp>
2018-11-29 20:33:43 +0900

    fix redux-logger

--- src/index.tsx
+++ src/index.tsx
@@ -32,9 +32,8 @@ const store = createStore(
   rootReducer(history),
   compose(
     applyMiddleware(...middlewares),
-    // ToDo redux devtools bugs
-    // (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-    //   (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+      (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
   ),
 );
 
