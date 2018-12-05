dc628b7b00257b9638fe00de17f849419a223ea6
yufrice <c1155208@cstu.nit.ac.jp>
2018-12-05 12:30:48 +0900

    uuooooo:

--- src/index.tsx
+++ src/index.tsx
@@ -31,11 +31,7 @@ if (process.env.NODE_ENV !== 'production') {
 const store: Store = createStore(
   rootReducer(history),
   compose(
-    applyMiddleware(...middlewares),
-    process.env.NODE_ENV !== 'production'
-      ? (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-          (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-      : undefined,
+    applyMiddleware(...middlewares)
   ),
 );
 
